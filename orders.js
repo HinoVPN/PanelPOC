ff0f1a8d1179d4cd23299c5082975b65556f7ada
Hino Ng <56183979+HinoVPN@users.noreply.github.com>
2022-04-07 22:55:00 +0800

    Add files via upload

---
+++ orders.js
@@ -0,0 +1,38 @@
+const Orders = [
+    {
+        productName: "Foldable Mini Drone",
+        productNumber: "856131",
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: "LARVENDER KF102 Drone",
+        productNumber: "857831",
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: "Ruko F11 Pro Drone",
+        productNumber: "856651",
+        paymentStatus: 'Due',
+        shipping: 'Declined'
+    },
+    {
+        productName: "Drone jai 2046",
+        productNumber: "857531",
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: "Foldable Mini Drone",
+        productNumber: "856131",
+        paymentStatus: 'Due',
+        shipping: 'Delivered'
+    },
+    {
+        productName: "Foldable Mini Drone",
+        productNumber: "858951",
+        paymentStatus: 'Due',
+        shipping: 'Delivered'
+    },
+]
